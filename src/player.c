e15aee5ac7de16e68e3ea9c2d7515da9eed9e221
padmalcom <j.freiknecht@googlemail.com>
2016-06-25 00:28:58 +0200

    Fixed movement code.

--- src/player.c
+++ src/player.c
@@ -35,24 +35,32 @@ action act_player() {
 		vForce.y = (key_a - key_d) * PLAYER_SPEED_E;
 		vForce.x = 0;
 		
-		//var dist = vec_dist(my.x, nullvector);
+		var dist = vec_dist(my.x, nullvector);
 		
-		//draw_text(str_for_num(NULL, dist), 10, 10, COLOR_RED);
 		
-		/*if (dist > LEVEL_LIMIT_RADIUS_E) {
-			vec_lerp(vForce, nullvector, vForce, 0.5);
-		}*/
+		// Keep the player in the middle of the screen
+		if (dist > LEVEL_LIMIT_RADIUS_E / 100 * 70) {
+
+			if ((my.y < 0) && (vForce.y < 0)) {
+				vForce.y = 0;
+			}
+			
+			if ((my.y > 0) && (vForce.y > 0)) {
+				vForce.y = 0;
+			}
+			
+			if ((my.z < 0) && (vForce.z < 0)) {
+				vForce.z = 0;
+			}
+			
+			if ((my.z > 0) && (vForce.z > 0)) {
+				vForce.z = 0;
+			}
+			
+		}
 		
+		// Accelerate movement
 		vec_accelerate(vecMoveSpeed, vSpeed, vForce, 0.2);
-		
-		// Keep player in screen
-		if ((vecMoveSpeed.z + my.z >= LEVEL_LIMIT_Z_E) || (vecMoveSpeed.z + my.z <= -LEVEL_LIMIT_Z_E)) {
-			vecMoveSpeed.z = 0;
-		}
-		
-		if ((vecMoveSpeed.y + my.y >= LEVEL_LIMIT_Y_E) || (vecMoveSpeed.y + my.y <= -LEVEL_LIMIT_Y_E)) {
-			vecMoveSpeed.y = 0;
-		}
 		
 		c_move(me, vecMoveSpeed.x, nullvector, IGNORE_PASSABLE | IGNORE_PASSENTS | IGNORE_ME | GLIDE | ACTIVATE_TRIGGER);
 		
