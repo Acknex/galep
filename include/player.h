b5f52d94c3431b0c8172c798120361bd052cc1b9
padmalcom <j.freiknecht@googlemail.com>
2016-06-25 00:09:54 +0200

    Added crosshair. Fixed movement.

--- include/player.h
+++ include/player.h
@@ -1,14 +1,17 @@
 #ifndef PLAYER_H_
 #define PLAYER_H_
 
-#define PLAYER_SPEED_E 1
-#define LEVEL_LIMIT_Z_E 8
-#define LEVEL_LIMIT_Y_E 10
+#define PLAYER_SPEED_E 50
+#define LEVEL_LIMIT_Z_E 500
+#define LEVEL_LIMIT_Y_E 800
 
-#define LEVEL_LIMIT_RADIUS_E 20
+#define LEVEL_LIMIT_RADIUS_E 1600
 
 void spawn_player();
 void act_player();
+
+BMAP* bmapCrosshair = "textures//crosshair.bmp";
+ENTITY* entCrosshair = NULL;
 
 
 #endif
--- src/player.c
+++ src/player.c
@@ -5,48 +5,45 @@ void spawn_player() {
 	
 	if (player != NULL) {
 		ent_remove(player);
+		ent_remove(entCrosshair);
 	}
-	player = ent_create("ufo.mdl", vector(20,0,0), act_player);
+	player = ent_create("ufo.mdl", vector(1000,0,0), act_player);
+	entCrosshair = ent_create("textures//crosshair.bmp", vector(1100, 0, 0), NULL);
+	//vec_scale(entCrosshair.scale_x, 1);
 	set(player, ENABLE_TRIGGER);
 	player.trigger_range = 20;
+}
+
+void move_crosshair() {
+	
 }
 
 action act_player() {
 	
 	VECTOR vecMoveSpeed;
-	VECTOR speed;
-	vec_set(speed, vector(0,1,1));
+	
+	VECTOR vSpeed, vMove, vForce;
+	
+	vec_zero(vSpeed);
+	vec_zero(vMove);
+	vec_zero(vForce);
 	
 	while(me) {
 		
-		// Speed up movement - or slow down
-		if (key_w || key_s) {
-			if (speed.z < 1.5) {
-				speed.z +=0.004;
-			}
-		} else {
-			if (speed.z > 1.0) {
-				speed.z -=0.004;
-			}
-		}
-		
-		if (key_a || key_d) {
-			if (speed.y < 1.5) {
-				speed.y +=0.004;
-			}
-		} else {
-			if (speed.y > 1.0) {
-				speed.y -=0.004;
-			}
-		}
-		
 		// Calculate player movement
-		vecMoveSpeed.z = (key_w - key_s) * time_step * PLAYER_SPEED_E * speed.z;
-		vecMoveSpeed.y = (key_a - key_d) * time_step * PLAYER_SPEED_E * speed.y;
+		vForce.z = (key_w - key_s) * PLAYER_SPEED_E;
+		vForce.y = (key_a - key_d) * PLAYER_SPEED_E;
+		vForce.x = 0;
 		
-		// Dont allow to move forward/backwards
-		vecMoveSpeed.x = 0;
-	
+		//var dist = vec_dist(my.x, nullvector);
+		
+		//draw_text(str_for_num(NULL, dist), 10, 10, COLOR_RED);
+		
+		/*if (dist > LEVEL_LIMIT_RADIUS_E) {
+			vec_lerp(vForce, nullvector, vForce, 0.5);
+		}*/
+		
+		vec_accelerate(vecMoveSpeed, vSpeed, vForce, 0.2);
 		
 		// Keep player in screen
 		if ((vecMoveSpeed.z + my.z >= LEVEL_LIMIT_Z_E) || (vecMoveSpeed.z + my.z <= -LEVEL_LIMIT_Z_E)) {
@@ -57,18 +54,11 @@ action act_player() {
 			vecMoveSpeed.y = 0;
 		}
 		
-		/*var dist = vec_dist(nullvector, vector(0, vecMoveSpeed.y + my.y, vecMoveSpeed.z + my.z));
+		c_move(me, vecMoveSpeed.x, nullvector, IGNORE_PASSABLE | IGNORE_PASSENTS | IGNORE_ME | GLIDE | ACTIVATE_TRIGGER);
 		
-		draw_text(str_for_num(NULL, dist), 10, 10, COLOR_RED);
-		
-		var factor = 100 / LEVEL_LIMIT_RADIUS_E * dist;
-		factor = (100 - factor) / 100;
-		
-		draw_text(str_for_num(NULL, factor), 10, 30, COLOR_RED);
-		
-		vec_lerp(vecMoveSpeed.x, nullvector, vecMoveSpeed.x, factor);*/
-		
-		c_move(me, vecMoveSpeed.x, nullvector, IGNORE_PASSABLE | IGNORE_PASSENTS | IGNORE_ME | GLIDE);
+		if (entCrosshair != NULL) {
+			c_move(entCrosshair, vecMoveSpeed.x, nullvector, IGNORE_PASSABLE | IGNORE_PASSENTS | GLIDE);
+		}
 		
 		camera_move();
 		wait(1);
